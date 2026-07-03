86f6e7a46aed2c7781c452b077ba8084bea4b294
Sam2404 <samarpan1645@gmail.com>
2026-07-03 13:49:19 +0530

    Add Task5

---
+++ Task5/src/bot_control/bot_control/fk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import math
+
+WHEEL_RADIUS = 0.14
+
+L = 0.68
+
+
+def forward_kinematics(omega_left, omega_right, omega_rear):
+
+    #write your implementation here
+
+    pass
+
+def main():
+
+    wl = float(input("Left wheel (rad/s): "))
+    wr = float(input("Right wheel (rad/s): "))
+    wb = float(input("Rear wheel (rad/s): "))
+
+    vx, vy, omega = forward_kinematics(
+        wl,
+        wr,
+        wb
+    )
+
+    print(f"\nvx     : {vx:.3f} m/s")
+    print(f"vy     : {vy:.3f} m/s")
+    print(f"omega  : {omega:.3f} rad/s")
+
+
+if __name__ == "__main__":
+    main()
